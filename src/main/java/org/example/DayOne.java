5d3b2283adf8f5bba81f7b83a10e25dd164398da
Stephanie Lopez <stephanieplopez@gmail.com>
2022-11-18 15:57:42 -0500

    Completed Day One part 1 and 2

---
+++ src/main/java/org/example/DayOne.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DayOne {
+
+    public static void main(final String args[]) {
+
+        List<Integer> expenseReport = new ArrayList<>();
+
+        String stringCurrentLine;
+        BufferedReader objReader = null;
+        int sum = 0;
+        int finalValue = 0;
+
+        //reads file in
+        try {
+            objReader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
+
+            //reads file in
+            while ((stringCurrentLine = objReader.readLine()) != null) {
+                expenseReport.add(Integer.parseInt(stringCurrentLine));
+            }
+
+            for(int i = 0; i < expenseReport.size() - 1; i++) {
+                int currentValue = expenseReport.get(i);
+
+                for (Integer value : expenseReport) {
+                    if (value + currentValue == 2020) {
+                        finalValue = value * currentValue;
+                        break;
+                    }
+                }
+            }
+            System.out.println(finalValue + " = is the product");
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
---
+++ src/main/java/org/example/DayOnePartTwo.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DayOnePartTwo {
+
+    public static void main(final String args[]) {
+
+        List<Integer> expenseReport = new ArrayList<>();
+
+        String stringCurrentLine;
+        BufferedReader objReader = null;
+        int sum = 0;
+        int finalValue = 0;
+
+        //reads file in
+        try {
+            objReader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
+
+            //reads file in
+            while ((stringCurrentLine = objReader.readLine()) != null) {
+                expenseReport.add(Integer.parseInt(stringCurrentLine));
+            }
+
+            for(int i = 0; i < expenseReport.size() - 2; i++) {
+                int currentValue = expenseReport.get(i);
+                for (int k = 0; k < expenseReport.size() - 2; k++) {
+                    int nextValue = expenseReport.get(k + 1);
+
+                    for (Integer value : expenseReport) {
+                        if (value + currentValue + nextValue == 2020) {
+                            finalValue = value * currentValue * nextValue;
+                            break;
+                        }
+                    }
+                }
+            }
+            System.out.println(finalValue + " = is the product");
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
